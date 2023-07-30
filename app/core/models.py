e9dcf653ca3ecebc3fbf0b8d4f3b9edee44341e2
Daria <42370945+ozerodar@users.noreply.github.com>
2023-07-30 10:43:50 +0200

    admin console

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,6 @@
 """Database models"""
 
-from django.db import models  # noqa
+from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 
