32ce2ae8acb82f428265ddb88ce9719f46196619
Daria <42370945+ozerodar@users.noreply.github.com>
2023-08-21 20:57:13 +0200

    update and delete tags

--- app/core/migrations/0004_auto_20230821_1735.py
+++ app/core/migrations/0004_auto_20230821_1735.py
@@ -6,23 +6,22 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('core', '0003_recipe'),
+        ("core", "0003_recipe"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Tag',
+            name="Tag",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=255)),
+                ("user", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
-            model_name='recipe',
-            name='tags',
-            field=models.ManyToManyField(to='core.Tag'),
+            model_name="recipe",
+            name="tags",
+            field=models.ManyToManyField(to="core.Tag"),
         ),
     ]
